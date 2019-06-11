34648172380f80ee7c649ccb53787adcd3e7e6ea
pikl91337 <khupeniya705@gmail.com>
2019-06-11 19:50:29 +0300

    GUI_with_tkinter_only_basics

---
+++ Chapter1/Step5/attachgui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter201 import MyGui
+
+# главное окно приложения
+mainwin = Tk()
+Label(mainwin, text=__name__).pack()
+
+# окно диалога
+popup = Toplevel()
+Label(popup, text='Attach').pack(side=LEFT)
+MyGui(popup).pack(side=RIGHT)
+mainwin.mainloop()
